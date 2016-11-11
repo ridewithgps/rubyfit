648fc70e0517da3d4f4494cc02c244149f46708b
Cullen King <cullen@ridewithgps.com>
2016-11-10 17:17:15 -0800

    Temp checkin

--- ext/rubyfit/fit_product.c
+++ ext/rubyfit/fit_product.c
@@ -17,26 +17,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-#if !defined(FIT_CRC_H)
-#define FIT_CRC_H
+#include "fit_product.h"
+#include "fit_example.c"
 
-#include "fit.h"
-
-
-//////////////////////////////////////////////////////////////////////////////////
-// Public Function Prototypes
-//////////////////////////////////////////////////////////////////////////////////
-
-#if defined(__cplusplus)
-   extern "C" {
-#endif
-
-FIT_UINT16 FitCRC_Get16(FIT_UINT16 crc, FIT_UINT8 byte);
-FIT_UINT16 FitCRC_Update16(FIT_UINT16 crc, const volatile void *data, FIT_UINT32 size);
-FIT_UINT16 FitCRC_Calc16(const volatile void *data, FIT_UINT32 size);
-
-#if defined(__cplusplus)
-   }
-#endif
-
-#endif // !defined(FIT_CRC_H)
